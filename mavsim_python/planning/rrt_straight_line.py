d98a3779f9ec53c7217769cb98838d8c2ce9bfac
Kody Shaw <shawk13013@gmail.com>
2023-04-07 19:21:15 -0600

    just working with the collision detection, smoother still needs tested.

--- mavsim_python/planning/rrt_straight_line.py
+++ mavsim_python/planning/rrt_straight_line.py
@@ -34,9 +34,10 @@ class RRTStraightLine:
             self.extend_tree(tree, end_pose, Va, world_map)
         
             pose_i = column(tree.ned, tree.num_waypoints-1)
-        waypoints = smooth_path(tree, world_map) # !!! this won't work when I have branches
+        waypoints = tree
         # find path with minimum cost to end_node
-        # waypoints_not_smooth = find_minimum_path()
+        waypoints = find_minimum_path(tree, end_pose)
+        # waypoints = smooth_path(waypoints, world_map) 
         return waypoints
 
     def extend_tree(self, tree, end_pose, Va, world_map):
@@ -46,75 +47,94 @@ class RRTStraightLine:
         # random_pose(world_map, Va) .... hmmmmm
 
         flag = True
-        while flag: # loop until a valid point is found ... or path is extended???? !!!
+        while flag: # loop until a valid point is found
             # Generate a random step
-            rand_x = np.random.uniform(0, 1)
-            if np.random.uniform(0, 1) < 0.5:
-                sign = 1
-            else:
-                sign = -1
-            rand_y = 1-rand_x
-            rand_x *= self.segment_length*sign
-            rand_y *= self.segment_length*sign
-            prev_point = tree.ned[:,tree.num_waypoints-1]
-            rand_step =  prev_point + np.array([rand_x, rand_y, 0])
+            rand_p = random_pose(world_map, end_pose.item(2))
+            # loop over all points in the tree to find closest point to rand_p_step
+            dist_list = []
+            for i in range(tree.num_waypoints):
+                dist_list.append(distance(column(tree.ned, i), rand_p))
+                idx = np.argmin(dist_list)
+            
+            prev_point = column(tree.ned, idx)
+            rand_p_step = (rand_p-prev_point)/np.linalg.norm(rand_p-prev_point) * self.segment_length
+            rand_step =  prev_point + rand_p_step            
+            dist = distance(rand_step, prev_point)
             # check to see if random point is close to end pose
-            if (distance(rand_step, end_pose) <= self.segment_length):
+            if (distance(rand_step, end_pose) <= self.segment_length/2):
+                dist = distance(rand_step, end_pose)
                 rand_step = end_pose
+                connect_to_goal = 1
+            else:
+                connect_to_goal = 0
+
             # check for collision
             flag = collision(prev_point, rand_step, world_map)
             # if there is no collision add point to the tree
             if (flag == False):
-                tree.add(np.array([rand_step]).T, Va, np.inf, np.inf, 0, 0) # !!! serious weakness this tree does not have any branches
+                tree.add(rand_step, airspeed=Va, course=np.inf, cost=dist, parent=idx, connect_to_goal=connect_to_goal)
         return flag
         
     def process_app(self):
         self.planner_viewer.process_app()
 
 def smooth_path(waypoints, world_map):
-
+    Va = waypoints.airspeed.item(0)
     # construct smooth waypoint path
     smooth_waypoints = MsgWaypoints()
     ##### TODO #####
     # smooth the waypoint path
-    smooth_waypoints.add(column(waypoints.ned,0), 0, np.inf, np.inf, 0, 0)  # add the first waypoint
-    for i in range(1, len(waypoints)-1):
+    smooth_waypoints.add(column(waypoints.ned,0), airspeed=Va, course=np.inf, cost=0, parent=0, connect_to_goal=0)  # add the first waypoint
+    j = 0
+    for i in range(1, waypoints.num_waypoints-1):
         # check for collision
-        flag = collision(waypoints[i-1], waypoints[i], world_map)
+        flag = collision(column(waypoints.ned,j), column(waypoints.ned,i), world_map)
         if (flag == True):
-            smooth_waypoints.add(column(waypoints,i-1), 0, np.inf, np.inf, 0, 0)
-
-    return smooth_waypoints
+            j = i-1 
+            smooth_waypoints.add(column(waypoints.ned,j), airspeed=Va, course=np.inf, cost=0, parent=j, connect_to_goal=0)
+    smooth_waypoints.add(column(waypoints.ned, waypoints.num_waypoints-1), airspeed=Va, course=np.inf, cost=0, parent=j, connect_to_goal=1)
     
-
 
     return smooth_waypoints
 
 
 def find_minimum_path(tree, end_pose):
     # find the lowest cost path to the end node
-
     ##### TODO #####
     # find nodes that connect to end_node
-    connecting_nodes = []
+    connecting_nodes = np.argmax(tree.connect_to_goal) # !!! this will break when finding more than one path
     
     # find minimum cost last node
-    idx = 0
+    idx = int(connecting_nodes) # !!! this will break when finding more than one path
 
     # construct lowest cost path order
-    path = []  # last node that connects to end node
+    path = [column(tree.ned, idx)] # last node that connects to end node
+    # loop untill we find the root 
+    while True:
+        parent = tree.parent[idx]
+        path.append(column(tree.ned, int(parent)))
+        idx = int(parent)
+        if idx == 0:
+            break
     
     # construct waypoint path
     waypoints = MsgWaypoints()
+    for i in range(len(path)-1):
+        idx = len(path)-1-i
+        
+        waypoints.add(np.array(path[idx]), airspeed=tree.airspeed.item(idx), course=np.inf, cost=0, parent=idx, connect_to_goal=0)
+
+    waypoints.add(np.array(path[0]), airspeed=tree.airspeed.item(0), course=np.inf, cost=0, parent=idx, connect_to_goal=1)
     return waypoints
 
 
 def random_pose(world_map, pd):
-    # generate a random pose
+    # generate a point within the city limits world_map.city_width
 
     ##### TODO #####
-    pn = 0
-    pe = 0
+    
+    pn = np.random.uniform(0, world_map.city_width)
+    pe = np.random.uniform(0, world_map.city_width)
     pose = np.array([[pn], [pe], [pd]])
     return pose
 
@@ -129,19 +149,23 @@ def distance(start_pose, end_pose):
 
 
 def collision(start_pose, end_pose, world_map):
+    # return False
     # check to see of path from start_pose to end_pose colliding with map
-    
     ###### TODO ######
     collision_flag = True
-    path_points = points_along_path(start_pose, end_pose, 30)
-    # for point in path_points:
-    #     if world_map[int(point[0]), int(point[1])] == 0:
-    #         collision_flag = False
-    #     else:
-    #         collision_flag = True !!! Still need to do this
-    collision_flag = False
+    path_points = points_along_path(start_pose, end_pose, 100)
+    #loop over all points between start and end pose and check for collision
 
-    return collision_flag
+    for point in path_points:
+        for ni, n_pose in enumerate(world_map.building_north):
+            for ei, e_pose in enumerate(world_map.building_east):
+                bld_pose = np.array([[n_pose.item(ni), e_pose.item(ei), point.item(2)]]).T
+                if (distance(point, bld_pose) < world_map.building_width*2.5):
+                    if (abs(point.item(2)) > abs(world_map.building_height[ni,ei])):
+                        # print("collision")
+                        return True
+    return False
+
 
 
 def height_above_ground(world_map, point):
@@ -154,9 +178,12 @@ def height_above_ground(world_map, point):
 def points_along_path(start_pose, end_pose, N):
     # returns points along path separated by Del
     unit_vector = (end_pose - start_pose)/(np.linalg.norm(end_pose-start_pose))
+    dist = distance(start_pose, end_pose)
+    res = dist/N
     points = []
     for i in range(N):
-        points.append(start_pose + i*unit_vector)
+        points.append(start_pose + i*res*unit_vector)
+
     return points
 
 
